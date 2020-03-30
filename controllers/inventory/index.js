5dc93963a36d74435aae3f058a308b48efceff9f
Theoderic <onipetheoderic@gmail.com>
2020-03-30 04:38:54 +0100

    all

--- controllers/inventory/index.js
+++ controllers/inventory/index.js
@@ -623,7 +623,7 @@ exports.verify_request = function(req, res){
                         User.findOne({_id:admin_1_verifier}, function(err, admin_1_guy){
                             const admin_assigned_user = admin_1_guy.user_detail;
 
-                            if(isReq){
+                            if(reqs.dept_director[0].toString() == decrypted_user_id){
                                 console.log("YYYY dept director")
                                 if(acceptance=="accept"){
                                     Request.findByIdAndUpdate(request_id, {
